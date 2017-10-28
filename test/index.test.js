f96023820cf4c09a99cf5691bb61bc421b1447c5
Madimetja Shika <mmjshika@gmail.com>
2017-10-28 14:51:49 +0200

    Updated the tests with the new changes in vuetify text fiels

--- test/index.test.js
+++ test/index.test.js
@@ -20,28 +20,40 @@ test('test that it renders a parent div tag with correct classes', t => {
     }}).$mount();
 
     t.is(vm.$el.nodeName, 'DIV');
-    t.is(vm.$el._prevClass, 'input-group input-group--dirty input-group--hide-details input-group--placeholder input-group--text-field input-group--single-line');
+    t.is(vm.$el._prevClass, 'input-group input-group--hide-details theme--light input-group--text-field primary--text');
 });
 
-test('test that it renders a label as child to parent div tag', t => {
+test('test that it renders an input-group div as child to parent div tag', t => {
     let Constructor = Vue.extend(VuetifyGoogleAutocomplete);
 
     let vm = new Constructor({ propsData: {
         'id': 'map'
     }}).$mount();
 
-    t.is(vm.$el.children[0].nodeName, 'LABEL');
+    t.is(vm.$el.children[0].nodeName, 'DIV');
+    t.is(vm.$el.children[0]._prevClass, 'input-group__input');
 });
 
-test('test that it renders an input tag as grandchild to parent div tag', t => {
+test('test that it renders an input-group details div as child to parent div tag', t => {
     let Constructor = Vue.extend(VuetifyGoogleAutocomplete);
 
     let vm = new Constructor({ propsData: {
         'id': 'map'
     }}).$mount();
 
-    t.is(vm.$el.children[1].children[0].nodeName, 'INPUT');
-    t.is(vm.$el.children[1].children[0].type, 'text');
+    t.is(vm.$el.children[1].nodeName, 'DIV');
+    t.is(vm.$el.children[1]._prevClass, 'input-group__details');
+});
+
+test('test that it renders a div tag as grandchild to parent div tag', t => {
+    let Constructor = Vue.extend(VuetifyGoogleAutocomplete);
+
+    let vm = new Constructor({ propsData: {
+        'id': 'map'
+    }}).$mount();
+
+    t.is(vm.$el.children[1].children[0].nodeName, 'DIV');
+    // t.is(vm.$el.children[1].children[0].type, 'text');
 });
 
 test('test that the input tag has the correct props', t => {
@@ -54,10 +66,9 @@ test('test that the input tag has the correct props', t => {
         'placeholder': 'Start typing'
     }}).$mount();
 
-    t.is(vm.$el.children[1].children[0].name, 'map');
-    t.is(vm.$el.children[1].children[0].id, 'map');
-    t.is(vm.$el.className, 'input-group input-group--dirty input-group--hide-details input-group--placeholder input-group--text-field input-group--single-line form-control');
-    t.is(vm.$el.children[1].children[0].placeholder, 'Start typing');
+    t.is(vm.$el.children[0].children[0].name, 'map');
+    t.is(vm.$el.children[0].children[0].id, 'map');
+    t.is(vm.$el.className, 'input-group input-group--hide-details input-group--placeholder theme--light input-group--text-field primary--text form-control');
 });
 
 test('test that the input is disabled if disabled prop is given as true', t => {
@@ -69,7 +80,7 @@ test('test that the input is disabled if disabled prop is given as true', t => {
         'disabled': true
     }}).$mount();
 
-    t.is(vm.$el.children[1].children[0].disabled, true);
+    t.is(vm.$el.children[0].children[0].disabled, true);
 });
 
 test('test that the input is not disabled if disabled prop is not given', t => {
@@ -80,5 +91,5 @@ test('test that the input is not disabled if disabled prop is not given', t => {
         'id': 'map',
     }}).$mount();
 
-    t.is(vm.$el.children[1].children[0].disabled, false);
+    t.is(vm.$el.children[0].children[0].disabled, false);
 });
