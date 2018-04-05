b03dc8528b1c41329048329fef116adbe54077e3
Maurice Schilpp <cweaveart@gmail.com>
2018-04-05 14:44:59 +0300

    This file simulates the down arrow key

---
+++ src/ult/simulateArrowDown.js
@@ -0,0 +1,25 @@
+export default (input) => {
+    var _addEventListener = (input.addEventListener) ? input.addEventListener : input.attachEvent;
+
+    function addEventListenerWrapper(type, listener) {
+        // Simulate a 'down arrow' keypress on hitting 'return' when no pac suggestion is selected,
+        // and then trigger the original listener.
+            var orig_listener = listener;
+            listener = function(event) {
+                var suggestion_selected = document.getElementsByClassName('pac-item-selected').length > 0;
+                if (event.which == 13 && !suggestion_selected) {
+                    var simulatedEvent = document.createEvent('Event');
+                    simulatedEvent.keyCode = 40;
+                    simulatedEvent.which = 40;
+                    orig_listener.apply(input, [simulatedEvent]);
+                }
+                orig_listener.apply(input, [event]);
+            };
+
+        _addEventListener.apply(input, [type, listener]);
+        console.log('hi from simulate');
+    }
+
+    input.addEventListener = addEventListenerWrapper;
+    input.attachEvent = addEventListenerWrapper;
+};
